0e22526f18cf1ccfc83ff030d39723c3bcb7443f
Tanish Agarwal <tanish@byldd.com>
2024-08-22 15:32:24 +0530

    chore: change game name

--- src/components/global/Navbar.tsx
+++ src/components/global/Navbar.tsx
@@ -1,13 +1,11 @@
-
 const Navbar = () => {
-  return (
-    <nav className='bg-[#E0E2DB] w-full'>
-      <div className='m-auto flex flex-row items-end py-4 w-max gap-3'>
-        <h1 className='text-3xl text-center'>Conway's Game of Life </h1>
-        <p className='text-base'>~ by Prasuk Jain</p>
-      </div>
-    </nav>
-  )
-}
+	return (
+		<nav className='bg-[#E0E2DB] w-full'>
+			<div className='m-auto flex flex-row items-end py-4 w-max gap-3'>
+				<h1 className='text-3xl text-center'>Life of Cells</h1>
+			</div>
+		</nav>
+	);
+};
 
 export default Navbar;
